22770ac108d8784ba13359b8e84fbcd1c3be3d44
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2020-01-10 16:59:54 +0100

    Added REST Api to the web project

---
+++ src/models/index.js
@@ -0,0 +1,36 @@
+let users = {
+    1: {
+        id: '1',
+        username: 'Kabir Abdulkareem',
+    },
+    2: {
+        id: '2',
+        username: 'Robin Wieruch',
+    },
+    3: {
+        id: '3',
+        username: 'Dave Davids',
+    },
+};
+let messages = {
+    1: {
+        id: '1',
+        text: 'Tales by moon light',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'Hello World',
+        userId: '2',
+    },
+    3: {
+        id: '3',
+        text: 'By World',
+        userId: '3',
+    },
+};
+
+export default {
+    users,
+    messages
+}
